161d10442983f8e39b559266e493c137df946697
DESKTOP-TE9JGFD\danie <danieljjs77@gmail.com>
2025-11-09 16:56:27 +0900

    Initial commit: Project setup

---
+++ src/main/java/com/isfx/shim/ShimApplication.java
@@ -0,0 +1,13 @@
+package com.isfx.shim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShimApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShimApplication.class, args);
+	}
+
+}
